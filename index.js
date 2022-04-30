789012b01abb7548c2e77beb7dbeddabfa10b457
Maurice Kevenaar <maurice@kevenaar.name>
2022-04-30 21:41:20 +0200

    (build) basic configuration

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { TOKEN } = process.env;
 const fs = require('fs');
 
 const client = new Client({
-	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MEMBERS],
 });
 
 // Commands Setup
